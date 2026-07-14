7cb40d76692c6a3a22600f7f9533b5f0a091065a
Sheras-art <s32742534@gmail.com>
2026-07-15 00:37:27 +0500

    Enhance createOrder logic to handle cart and buyNow checkout types; add validation for cart items and update stock management; improve error handling for order creation and removing ordered products from cart

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -13,6 +13,12 @@ const createOrder = asyncHandler(async (req, res) => {
     // if from buyNow then take the product id and search for product in db and create order
     // send response to fornt end with important details like created order deatils to frontend
 
+
+    if (!req.user) {
+        throw new apiError(401, "User not authenticated");
+    }
+
+    const userId = req.user._id;
     const { items, type, paymentMethod, shippingAddress } = req.body;
     console.log(type);
 
@@ -22,12 +28,11 @@ const createOrder = asyncHandler(async (req, res) => {
     if (type !== "cart" && type !== "buyNow") {
         throw new apiError(400, "Invalid type value");
     }
-    if (!items || items.length === 0) {
+    if (type === "buyNow" && (!items || items.length === 0)) {
         throw new apiError(400, "Order items are required");
     }
 
     console.log(type);
-    
 
     if (type === "buyNow") {
         const orderItems = await Promise.all(
@@ -52,12 +57,8 @@ const createOrder = asyncHandler(async (req, res) => {
             })
         )
 
-        if (!req.user) {
-            throw new apiError(401, "User not authenticated");
-        }
-
         const order = await Order.create({
-            user: req.user._id,
+            user: userId,
             items: orderItems.map(item => ({
                 product: item.product._id,
                 quantity: item.quantity,
@@ -81,18 +82,18 @@ const createOrder = asyncHandler(async (req, res) => {
         }
 
         await Promise.all(
-            orderItems.map(async (item) => {
+            orderItems.map((item) => (
                 Product.findByIdAndUpdate(
                     item.product._id,
                     {
-                        $inc: {stock: -item.quantity}
+                        $inc: { stock: -item.quantity }
                     }
-                )
-            })
+                ))
+            )
         )
 
         return res.status(201).json(new apiResponse(201, order, "Order created successfully"));
-        
+
     } else if (type === "cart") {
         // Handle cart checkout logic here
         // get order details from req.body
@@ -100,13 +101,82 @@ const createOrder = asyncHandler(async (req, res) => {
         // get user cart from db by user id
         // get all essential details from cart and create order
 
-        const userId = req.user._id;
 
-        const cart = await Cart.findOne({ user: userId });
+        const cart = await Cart.findOne({ user: userId }).populate('products.product');
 
-        console.log(cart);
+        if (!cart) {
+            throw new apiError(404, "Cart not found");
+        }
+        if (cart.products.length === 0) {
+            throw new apiError(400, "Cart is empty")
+        }
 
-        res.status(201).json(new apiResponse(201, "working as i think", "Order created successfully✅"))
+        for (const item of cart.products) {
+
+            if (!item.product) {
+                throw new apiError(
+                    404,
+                    "Some products in your cart no longer exist"
+                );
+            }
+
+            if (item.product.stock < item.quantity) {
+                throw new apiError(400, `${item.product.title} has insufficient stock`);
+            }
+
+            if (item.product.isActive === false) {
+                throw new apiError(
+                    400,
+                    `${item.product.title} is not available`
+                );
+            }
+        }
+
+        const order = await Order.create({
+            user: userId,
+            items: cart.products.map((item) => ({
+                product: item.product._id,
+                quantity: item.quantity,
+                price: item.product.price,
+                title: item.product.title,
+                coverImage: item.product.coverImage,
+                category: item.product.category
+            })),
+            totalAmount: cart.products.reduce((acc, item) => (
+                acc + item.product.price * item.quantity
+            ), 0),
+            shippingAddress: shippingAddress,
+            paymentMethod: paymentMethod,
+        });
+
+        if (!order) {
+            throw new apiError(500, "Failed to create order");
+        }
+
+        // revising the stock after successfull order creation
+        await Promise.all(
+            cart.products.map((item) => (
+                Product.findByIdAndUpdate(
+                    item.product._id,
+                    {
+                        $inc: { stock: -item.quantity }
+                    }
+                )
+            ))
+        )
+
+        // removing the specific products(which comes from frontend) from cart
+
+        await Cart.findOneAndUpdate({ user: userId },
+            {
+                $pull: {
+                    products: { product: { $in: cart.products.map(item => item.product._id) } }
+                }
+            }
+        )
+
+        res.status(201).json(new apiResponse(201, order, "Order created successfully✅"));
+
     } else {
         throw new apiError(400, "invalid checkout type!")
     }
